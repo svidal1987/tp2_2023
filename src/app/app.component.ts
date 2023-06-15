1f5e6118ce740dd3213a37748a53cca77eb5d3cd
emiliano1995 <emiliano_roldan@hotmail.com>
2023-06-15 09:50:28 -0300

    Se agrego una tarjeta mostrando el user logueado en la pestania grupo2 y ademas en appcomponent se agrego el post para mandar al api los datos del usuario logueado para su verificacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,33 @@ import { AuthService } from '@auth0/auth0-angular';
 export class AppComponent {
   title = 'tp2_2023';
   isLogin = false
-  constructor(public auth: AuthService){
+  constructor(public auth: AuthService, private http: HttpClient){
     this.auth.isAuthenticated$.subscribe(isAuthenticated => {
       if(isAuthenticated){
         this.isLogin = true
       }
     })
+
+    this.auth.user$.subscribe((authResult) => {
+      const user = {
+        firstName: authResult?.given_name,
+        lastName: authResult?.family_name,
+        username: authResult?.email,
+      };
+      
+      // Enviar el objeto de usuario a tu API
+      const url = 'https://ejemplo.com/api/endpoint';
+      const headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+      this.http.post(url, user, { headers }).subscribe(
+        (response) => {
+          console.log('Solicitud exitosa', response);
+        },
+        (error) => {
+          console.error('Error en la solicitud', error);
+        }
+      );
+    });
   }
 
   login(){
